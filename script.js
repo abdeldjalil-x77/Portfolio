9d3a9caf74eee681e6e1feb51f30a3e4fbbe2365
abdeldjalil-x77 <abdeldjaliltounsi932@gmail.com>
2025-06-23 00:21:27 +0200

    first commit

--- script.js
+++ script.js
@@ -28,4 +28,49 @@ function revealOnScroll() {
   window.addEventListener('scroll', revealOnScroll);
   // وتشغيلها عند بداية تحميل الصفحة
   window.addEventListener('load', revealOnScroll);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbyLgYWp6X4DPegKmHCdCCxWz770_FdKngaOU_77VvCnFfM54w3HSeFrTasvUgohFYAp/exec';
+
+  document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+  
+    const name = this.querySelector('[name="name"]').value;
+    const email = this.querySelector('[name="email"]').value;
+    const message = this.querySelector('[name="message"]').value;
+  
+    const formData = { name, email, message };
+  
+  
+    fetch(scriptURL, {
+      method: 'POST',
+      body: JSON.stringify(formData)
+    })
+    .then(response => {
+      alert("✅ Message sent successfully!");
+      this.reset();
+    })
+    .catch(error => {
+      alert("❌ Failed to send message: " + error.message);
+    });
+  });
+  
+  
+
   
